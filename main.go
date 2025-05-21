610caa334d13d3964311831cd690772622dddf0c
Anton_Wombat <tagan20011002@yandex.ru>
2025-05-21 14:20:39 +0300

    second commit

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-	const UsdToEur = 1.13
-	const UsdToRub = 80
+const UsdToEur = 1.13
+const UsdToRub = 80
 
+func main() {
+
+	getUserInput()
 	eurToRub := UsdToEur * UsdToRub
 	fmt.Println(eurToRub)
+}
+
+func getUserInput() {
+	var input string
+	fmt.Scan(&input)
+}
+
+func converter(rub float64, eur float64, usd float64) float64 {
+	return 0.0
 }
